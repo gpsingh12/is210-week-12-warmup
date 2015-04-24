cc5aa12e43420bb3c547de1cda7eaa9e553e6507
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-24 22:53:19 +0000

    age

--- task_02.py
+++ task_02.py
@@ -7,8 +7,28 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """ Exception is parent class for InvalidAgeError.
+        Check invalid age and raise an error
+    Attributes:
+        None
+    """
     pass
 
+
 def get_age(birthyear):
+    """Function convert birth year to age.
+    Args:
+        birthyear(int): input value of birthyear to be converted.
+    Returns:
+        returns age, if birthyear is valid, otherwise return error.
+    Examples:
+        >>> get_age(2099)
+        Traceback (most recent call last):
+            File "<stdin>"' line 1, in <module>
+        __main__.InvalidAgeError
+    """
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age >= 0:
+        return age
+    else:
+        raise InvalidAgeError
