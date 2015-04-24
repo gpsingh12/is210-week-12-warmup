cd061953be76d819d92c484bd43242328edba0eb
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-24 22:52:52 +0000

    lookup

--- task_01.py
+++ task_01.py
@@ -4,4 +4,20 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """Function for exception.
+        Args:
+            var1:input value for function
+            var2:input value for function
+        Returns:
+            returns an error message if invalid key or index is accessed.
+        Examples:
+            >>> simple_lookup([1,2], 4)
+            Warning: Your index/key doesn't exist
+            >>> simple_lookup({}, 'banana')
+            Warning: Your index/key doesn't exist.
+    """
+    try:
+        return var1[var2]
+    except (KeyError, IndexError):
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
